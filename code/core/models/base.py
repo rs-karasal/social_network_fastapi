4bc8d45d8c8330eb16135722abb4a09cd540a181
millennial <ereskarasal94@gmail.com>
2024-07-06 12:19:59 +0500

    created skeleton

--- code/core/models/base.py
+++ code/core/models/base.py
@@ -1,5 +1,4 @@
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import declared_attr
 
 from utils import camel_case_to_snake_case
@@ -12,6 +11,5 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
 
 
